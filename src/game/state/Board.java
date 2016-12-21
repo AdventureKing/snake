425b37bf6eff5b31fbd831557b9a789b373a760d
fernando.c.garcia <fernando.c.garcia@AFSWWBE6IPS23Y.us.accenturefederal.com>
2016-12-21 10:44:25 -0600

    Implement the Board class

--- src/game/state/Board.java
+++ src/game/state/Board.java
@@ -22,6 +22,7 @@ import javax.swing.Timer;
 public class Board extends JPanel implements ActionListener {
 
 
+	
     public static final int WIDTH = 600;
     public static final int HEIGHT = 400;
     
